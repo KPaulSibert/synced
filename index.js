eb904b1edc8c42c5f9c34100e46bbd8b63362baa
KPaulSibert <paul.sibert@kohlschein.de>
2021-11-10 21:08:17 +0100

    prealpha

--- index.js
+++ index.js
@@ -5,49 +5,57 @@ export function SymbolScope(name){
         }
     })
 }
-export const cfg = SymbolScope('cfg')// object configuration
-export const pcfg = SymbolScope('pcfg')// property/function configuration
-export const init = SymbolScope('init') // init functions(constructor)
-export const msg = SymbolScope('msg')// message handlers
-export const fromResp = {
-   async str(r){return await r.text()},
-   async json(r){return await r.json()}
-}
-export const toReq = {
-    str({path,body}){return fetch(path,{method:"POST",body})},
-    json({path,value}){return fetch(path,{method:"POST",body:JSON.stringify(value)})},
-    pyargs({path,args,kwargs}){return toReq.json({path,value:[args,kwargs]})}
-}
-export const parser = {
-    table(){},
-    obj_info(dct,opts){
-        const inp = toReq[dct.inp||'pyargs']
-        const out = fromResp[dct.out||'json']
-        const parser = parser[dct.post]
-        function fn(...args){
-           return send({...opts,inp,out,parser,args})
+/**@param {Server} srv */
+export function table(srv,data){
+    function parseTable(data){
+        let {fields,values,type,...other} = data;
+        type = srv.getType(type);
+        Object.assign(type,other)
+        for(const [id,arr] of Object.entries(values)){
+            srv.updateObj(type,id,Object.fromEntries(fields.map((e,i)=>[e,arr[i]])))
         }
     }
+    if(Array.isArray(data)){return data.map(e=>parseTable(data))}
+    else{return parseTable(data)}
 }
-const defOpts = {port:2020,prefix:'/api',inp:toReq.pyargs,out:fromResp.json,path:''}
-export async function send(opts={}){
-    const {path,port,prefix,host,inp,out,parser,...other} = {...defOpts,...opts}
-    // build urlpath
-    if(host){prefix = `http://${host}:${port}${prefix}`}
-    opts = {path:prefix+(path||''),...other}
-    // handle request
-    let ret = await out(await inp(opts),opts)
-    if(parser){ret = await parser(ret,opts)}
-    return ret
-    
-}
-export default function remote(obj){
-    return new Proxy(obj,{
-        get(tg,key){
-            if(key instanceof Symbol&&type[key]){return type[key]}
-            const handler = type[key]||type[cfg.pdef]
-            if(handler?.get){return handler.get(tg,key)}
-            else{return stg[key]}
+export const web = SymbolScope('web')
+
+export default class Server{
+    constructor(url=""){
+        this.url = url
+    }
+    add(types={}){
+        Object.assign(this.types,types);
+        for(const [tname,type] of Object.entries(types)){
+            if(type.objects==undefined){type.objects = {}}
+            for(const symbol of Object.getOwnPropertySymbols(type)){
+                const [scope,name] = symbol.description.split(':')
+                if(scope=='web'){
+                    const cb = type[symbol]
+                    type[name] = webmethod(this,tname,name,cb)
+                }
+            }
         }
-    })
+    }
+    updateObj(type,id,data){
+        if(id in type.objects){Object.assign(type.objects[id],data)}
+        else{data.id = id;type.objects[id] = data}
+        return
+    }
+    getType(name){return this.types[name]}
+    types = {}
+    url=""
+}
+export function webmethod(server,type,name,cb){
+    const fn = async function(args){
+        const req = await fetch(`${server.url}/${type}/${name}`,{method:'POST',body:JSON.stringify(args)})
+        let result = await req.json()
+        if(cb){
+            const modifed = cb(result)
+            if(modifed!=undefined){result = modifed}
+        }
+        return result
+    }
+    Object.defineProperty(fn, 'name', {value: name, writable: false});
+    return fn
 }

16197398b4ede2db7981dd4378b89af9720dadd2
Paul Sibert <paul.sibert@kohlschein.de>
2021-11-30 16:27:19 +0100

    namespace

--- index.js
+++ index.js
@@ -45,145 +45,133 @@ export const fld = SymbolScope('field')
 export const ctr = SymbolScope('constructor')
 export const cfg = SymbolScope('config')
 export const on = SymbolScope('event')
-export function newClass(name,ext){
+// dynamically define Class (with Base class)
+export function newClass(name,base){
     const classGener = new Function('ext',`return class ${name} extends ext{}`)
-    return classGener(ext)
+    return classGener(base)
 }
-export function setupType(type,opts={}){
+// iterates all Symbols in class (and class prototype) and calls its handlers
+export function setupClass(type,handlers={},opts={}){
     for(const [scope,name,val,tg] of iterSymbols(type)){
-        if(!(scope in setupType.handlers)){continue}
-        setupType.handlers[scope](tg,name,{val,isProto:false,...opts})
+        if(!(scope in handlers)){continue}
+        handlers[scope](tg,name,{val,isProto:false,...opts})
     }
     for(const [scope,name,val,tg] of iterSymbols(type.prototype)){
-        if(!(scope in setupType.handlers)){continue}
-        setupType.handlers[scope](tg,name,{val,isProto:true,...opts})
+        if(!(scope in handlers)){continue}
+        handlers[scope](tg,name,{val,isProto:true,...opts})
     }
+    return opts
 }
-function remote(tg,name,{val:cb,isProto}){
-    tg[name] = async function(args){
-        const type = isProto?this.constructor:this
-        const src = this[cfg.ns][cfg.src]
-        if(isProto){args = [this.id,args]}
-        const req = await fetch(`${src}/${type.name}/${name}`,{method:'POST',body:JSON.stringify(args)})
-        let result = await req.json()
-        result = server.fromJSON(result)
-        if(cb){
-            const modifed = cb.apply(this,[result])
-            if(modifed!=undefined){result = modifed}
+// Symbol listeners for Namespace binding 
+export const bindHandlers = {
+    remote(tg,key,{val:cb,isProto}){
+        tg[key] = async function(args){
+            const type = isProto?this.constructor:this
+            const src = this[cfg.ns][cfg.src]
+            if(isProto){args = [this.id,args]}
+            const req = await fetch(`${src}/${type.name}/${key}`,{method:'POST',body:JSON.stringify(args)})
+            let result = await req.json()
+            result = server.fromJSON(result)
+            if(cb){
+                const modifed = cb.apply(this,[result])
+                if(modifed!=undefined){result = modifed}
+            }
+            return result
         }
-        return result
     }
 }
 export class Namespace{
-    constructor(src){this[cfg.src]=src}
+    constructor(name,types={},src){
+        this[cfg.name] = name
+        this[cfg.add](types)
+        this[cfg.src]=src || '/'+name
+    }
     [cfg.add](types){
         for(const [tname,type] of Object.entries(types)){
-            const btype = types[tname] = newClass(tname,type);
-            btype[cfg.ns] = this
-            btype.prototype[cfg.ns] = this
+            // bound type (with Namespace)
+            let boundType = newClass(`${this[cfg.name]}_${tname}`,type);
+            // add namespace reference
+            boundType[cfg.ns] = this
+            boundType.prototype[cfg.ns] = this
+            const ctx = {type:boundType}
+            ({type:boundType} = setupClass(boundType,bindHandlers,ctx));
+            types[tname] = boundType
         }
         Object.assign(this,types)
         return types
     }
 }
-export default class Server{
-    constructor(url=""){
-        this.url = url
-    }
-    bindType(type,name){
-        const ctrs = {} //constructors
-        const newType = newClass({name,[cfg.remote]:this},type,ctrs)
-        newType.prototype[cfg.remote] = this
-        setupType(newType,{ctrs,newType})
-        if(type[on.extend]){type[on.extend](newType,newType)}
-        return newType
-    }
-    add(types={}){
-        for(const [tname,type] of Object.entries(types)){
-            types[tname] = this.bindType(type,tname)
-        }
-        Object.assign(this.types,types)
-        return types
-    }
-    fromJSON(data){
-        if(data!=null && typeof data =="object"){
-            for(const key of Object.keys(data)){
-                data[key] = this.fromJSON(data[key])
-            }
-            if(data.$t){
-                const type = this.types[data.$t]
-                if(!type||!type[cfg.fromJSON]){
-                    throw Error(`${data.$t} type or his 'fromJSON' handler wasnt found`)}
-                delete data.$t
-                return type[cfg.fromJSON](data)
-            }
-        }
-        return data
-    }
-    getType(data){
-        if(Array.isArray(data)){
-            const [name,...args] = data
-            const type = this.types[name]
-            if(!type)return;
-            const fn = type.generic || ((o)=>({...o,__proto__:type}));
-            return fn(...args)
-        }else{return this.types[data]}
-    }
-    types = {}
-    url = ""
-}
 export class Model{
-    static [rmt.all](){}
-    [rmt.update](){} 
     static vals(){return Object.values(this.objects)}
-    static parseField(val){return this.provideId(val)}
-    static dumpField(val){return val.id}
     static provideId(id){
         return id in this.objects?this.objects[id]:(this.objects[id] = Object.assign(new this(id),{id}))
     }
+    // remote Functions
+    static [rmt.all](){}
+    [rmt.update](){} 
+    // Events
+    static [on.bindNS](cls){cls.objects = {}}
+    // Serialization
     static [cfg.fromJSON](data){
-            let {fields,values,...other} = data;
-            Object.assign(this,other)
-            if(!values){return}
-            for(const [id,arr] of Object.entries(values)){
-                if(arr==null){console.log(id)
-                    delete this.objects[id];continue}
-                const obj = {}
-                for(const [i,val] of Object.entries(arr)){
-                    const field = fields[i]
-                    const ftype = this.prototype[fld[field]]
-                    if(ftype?.parseField){
-                        const parser = ftype.parseField
-                        obj[field] = parser.apply(ftype,[val,this,field])
-                    }else{
-                        obj[field] = val
-                    }
+        let {fields,values,...other} = data;
+        Object.assign(this,other)
+        if(!values){return}
+        for(const [id,arr] of Object.entries(values)){
+            if(arr==null){console.log(id)
+                delete this.objects[id];continue}
+            const obj = {}
+            for(const [i,val] of Object.entries(arr)){
+                const field = fields[i]
+                const ftype = this.prototype[fld[field]]
+                if(ftype?.parseField){
+                    const parser = ftype.parseField
+                    obj[field] = parser.apply(ftype,[val,this,field])
+                }else{
+                    obj[field] = val
                 }
-                Object.assign(this.provideId(id),obj)
             }
+            Object.assign(this.provideId(id),obj)
+        }
     }
-    static [on.extend](cls){cls.objects = {}}
+    static [cfg.parseField](val){return this.provideId(val)}
+    static [cfg.dumpField](val){return val.id}
 }
-export function dumpInst(obj){
+// search objects with "$t" property and parse them
+export function processJSON(data,namespace){
+    if(data!=null && typeof data =="object"){
+        for(const key of Object.keys(data)){
+            data[key] = this.fromJSON(data[key])
+        }
+        if(data[ "$t" ]){
+            const type = namespace[data.$t]
+            if(!type||!type[cfg.fromJSON]){
+                throw Error(`${data.$t} type or his 'fromJSON' handler wasnt found`)}
+            delete data.$t
+            return type[cfg.fromJSON](data)
+        }
+    }
+    return data
+}
+export function toJSON(obj){
     const ret = {}
     for(const [key,val] of Object.entries(obj)){
-        const ftype = obj[fld[key]]
-        if(ftype?.dumpField){
-            ret[key] = ftype.dumpField(val)
+        const fieldDescriber = obj[fld[key]]
+        if(fieldDescriber?.[cfg.dumpField]){
+            ret[key] = fieldDescriber[cfg.dumpField](val,obj,key)
         }
+        // if val is not Object
         else if(val==null || !['object','function'].includes(typeof val)){
             ret[key] = val
         }
     }
     return ret
-    
 }
 // types
-export class jsonType{
-    static parseField(val){ return JSON.parse(val)}
-    static dumpField(val){ return JSON.stringify(val)}
+export const jsonField = {
+    [cfg.parseField](val){ return JSON.parse(val)},
+    [cfg.dumpField](val){ return JSON.stringify(val)}
 }
-export class dateType{
-    static parseField(val){ return val && new Date(val)}
-    static dumpField(val){return val&&val.toISOString().substr(0,10)}
+export const dateField = {
+    [cfg.parseField](val){ return val && new Date(val)},
+    [cfg.dumpField](val){return val&&val.toISOString().substr(0,10)}
 }
